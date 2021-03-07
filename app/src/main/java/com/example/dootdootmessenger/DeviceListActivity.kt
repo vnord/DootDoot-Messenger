445f9b8ed156e8d94716c539b144be62f57cdc12
vnord <ari@vnord.net>
2021-03-07 19:43:50 +0100

    Add basic device scanning

--- app/src/main/java/com/example/dootdootmessenger/DeviceListActivity.kt
+++ app/src/main/java/com/example/dootdootmessenger/DeviceListActivity.kt
@@ -2,15 +2,30 @@ package com.example.dootdootmessenger
 
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothDevice
-import androidx.appcompat.app.AppCompatActivity
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
 import android.widget.ArrayAdapter
 import android.widget.ListView
+import android.widget.ProgressBar
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
 
 class DeviceListActivity : AppCompatActivity() {
 
+    private val context: Context = this
+
     private lateinit var listPairedDevices: ListView
     private lateinit var listAvailableDevices: ListView
+    private lateinit var progressScanDevices: ProgressBar
+
+
     private lateinit var adapterPairedDevices: ArrayAdapter<String>
     private lateinit var adapterAvailableDevices: ArrayAdapter<String>
 
@@ -25,6 +40,7 @@ class DeviceListActivity : AppCompatActivity() {
     private fun init() {
         listPairedDevices = findViewById(R.id.list_paired_devices)
         listAvailableDevices = findViewById(R.id.list_available_devices)
+        progressScanDevices = findViewById(R.id.progress_scan_devices)
 
         adapterPairedDevices = ArrayAdapter(this, R.layout.device_list_item)
         adapterAvailableDevices = ArrayAdapter(this, R.layout.device_list_item)
@@ -40,5 +56,65 @@ class DeviceListActivity : AppCompatActivity() {
             )
         }
 
+        registerReceiver(
+            bluetoothDeviceListener,
+            IntentFilter(BluetoothDevice.ACTION_FOUND)
+        )
+        registerReceiver(
+            bluetoothDeviceListener,
+            IntentFilter(BluetoothAdapter.ACTION_DISCOVERY_FINISHED)
+        )
+
+    }
+
+    private val bluetoothDeviceListener: BroadcastReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent) {
+            when (intent.action) {
+                BluetoothDevice.ACTION_FOUND -> {
+                    val device: BluetoothDevice? =
+                        intent.getParcelableExtra<BluetoothDevice>(BluetoothDevice.EXTRA_DEVICE)
+                    if (device!!.bondState != BluetoothDevice.BOND_BONDED) {
+                        adapterAvailableDevices.add(device.name + "\n" + device.address)
+                    }
+                }
+                BluetoothAdapter.ACTION_DISCOVERY_FINISHED -> {
+                    progressScanDevices.visibility = View.GONE
+                    if (adapterPairedDevices.count == 0) {
+                        Toast.makeText(context, "No new devices found", Toast.LENGTH_SHORT).show()
+                    } else {
+                        Toast.makeText(context, "Clicked on device", Toast.LENGTH_SHORT).show()
+                    }
+                }
+
+            }
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_device_list, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.menu_scan_devices -> {
+                scanDevices()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun scanDevices() {
+        progressScanDevices.visibility = View.VISIBLE
+        adapterAvailableDevices.clear()
+
+        Toast.makeText(this, "Scan started", Toast.LENGTH_SHORT).show()
+
+        if (bluetoothAdapter.isDiscovering) {
+            bluetoothAdapter.cancelDiscovery()
+        }
+
+        bluetoothAdapter.startDiscovery()
     }
 }
